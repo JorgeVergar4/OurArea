18fae4d0ff0da956ce04d6d6fc8a43616b293a8c
Boryot <jvstuardo@gmail.com>
2025-10-13 22:41:17 -0300

    create event model

--- app/src/main/java/cl/duoc/ourarea/model/Event.kt
+++ app/src/main/java/cl/duoc/ourarea/model/Event.kt
@@ -1,11 +1,16 @@
 package cl.duoc.ourarea.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "events")
 data class Event(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
+    val latitude: Double,
+    val longitude: Double,
+    val image: String,
     val description: String,
-    val imageRes: Int,
-    val distanceText: String,
-    val infoText: String
+    val distance: Double,
+    val timeInfo: String
 )

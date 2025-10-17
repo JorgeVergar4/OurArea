8bb4758372d32d447ae1bdeecf9398959a23813e
Boryot <jvstuardo@gmail.com>
2025-10-17 19:40:46 -0300

    modifier eventmodel

--- app/src/main/java/cl/duoc/ourarea/model/Event.kt
+++ app/src/main/java/cl/duoc/ourarea/model/Event.kt
@@ -2,15 +2,25 @@ package cl.duoc.ourarea.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Ignore
 
 @Entity(tableName = "events")
 data class Event(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
+    val description: String,
     val latitude: Double,
     val longitude: Double,
     val image: String,
-    val description: String,
-    val distance: Double,
-    val timeInfo: String
-)
+    val timeInfo: String,
+    val isFree: Boolean = false,
+    val isFamily: Boolean = false,
+    val isMusic: Boolean = false,
+    val isFood: Boolean = false,
+    val isArt: Boolean = false,
+    val isSports: Boolean = false
+) {
+    @Ignore
+    var distance: Float = 0f
+}
+
